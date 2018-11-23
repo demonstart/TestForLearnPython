029f3c006c76c5ac2c781c82b37745ccd80929bc
demonstart <demonstart@163.com>
2018-11-23 16:59:02 +0800

    第三次

--- test1.py
+++ test1.py
@@ -1,7 +1,20 @@
 #!./usr/bin/env.python
-#.-*-.coding:.utf-8.-*-
+#.-*-.coding:utf-8
 #.__author__:."liulifeng"
 #.Date:.2018-11-23
 
-c = 1
-print(c)
+i = 0
+while i < 3:
+    name = input('enter your name : ')
+    pwd = input('enter your password : ')
+    if name == '1' and pwd == '2':
+        print('ok')
+        break
+    else:
+        print('error')
+    i += 1
+
+print('test')
+
+
+
